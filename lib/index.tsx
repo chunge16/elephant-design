93e2500be23c3ff84d1d806485cad9b5d44da52b
chunge <1362598656@qq.com>
2019-10-17 01:17:22 +0800

    feat: :sparkles: 初次添加webpack配置文件

---
+++ lib/index.tsx
@@ -0,0 +1,3 @@
+const div = document.createElement('div');
+div.innerText = 'hello, world, hi';
+document.body.appendChild(div);
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        index: './lib/index.tsx'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist/lib'),
+        library: 'elephantDesign',
+        libraryTarget: 'umd'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack App'
+        })
+    ]
+};
