be813717b86c160d6e9e0ba4837998591045f3c3
chunge <1362598656@qq.com>
2019-10-24 00:14:31 +0800

    feat: :sparkles: 添加组件Icon

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Button from './Button/index';
+import Icon from './Icon/index';
 
 function App() {
     return (
         <div>
             <h1>刘德华</h1>
-            <Button />
+            <Button/>
+            <Icon name='wechat'/>
         </div>
     );
 }
