67f992100e1fe73d6cb2a42e701324c8b23a1cd5
chunge <1362598656@qq.com>
2019-12-03 00:19:36 +0800

    feat: :sparkles: 首次发布npm代码

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,21 +1,3 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import Button from './Button/button';
-import Icon from './Icon/icon';
+import './index.scss';
+export {default as Icon} from './icon/icon';
 
-function App() {
-    return (
-        <div>
-            {/*<h1>刘德华</h1>*/}
-            <Button/>
-            <Icon
-                name='wechat'
-                onMouseEnter={() => console.log('enter')}
-                onMouseLeave={() => console.log('leave')}
-                onTouchStart={() => console.log('touch')}
-            />
-        </div>
-    );
-}
-
-ReactDom.render(<App/>, document.getElementById('root'));
